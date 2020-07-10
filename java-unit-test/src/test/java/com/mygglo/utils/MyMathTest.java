2bebb15c4b9b10069b3ba67ec1987d4e13e40ac7
James Gaglo <freemanpolys@gmail.com>
2020-07-10 11:30:24 +0000

    Junit with java

--- java-unit-test/src/test/java/com/mygglo/utils/MyMathTest.java
+++ java-unit-test/src/test/java/com/mygglo/utils/MyMathTest.java
@@ -1,12 +1,53 @@
 package com.mygglo.utils;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MyMathTest {
 
-    @Test
-    void sum() {
+    private int[] arrangeNumbers;
+
+    @BeforeEach
+    void setUp() {
+        // Arrange
+        System.out.println("Before");
+        arrangeNumbers = new int[]{5, 2, 3};
     }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After");
+        arrangeNumbers = null;
+    }
+
+    @Test
+    public void sumMustBeEqualToSix() {
+        System.out.println("== sumMustBeEqualToSix ===");
+        //Arrange
+        int[] numbers = new int[]{1, 2, 3};
+
+        // Act
+        MyMath myMath = new MyMath();
+        int result = myMath.sum(numbers);
+
+        //Assertion
+         assertThat(result).isNotNull();
+         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void sumMustBeGreaterThanFour() {
+        System.out.println("== sumMustNotBeEqualToFour ===");
+        // Act
+        MyMath myMath = new MyMath();
+        int result = myMath.sum(arrangeNumbers);
+
+        //Assertion
+        assertThat(result).isNotNull();
+        assertThat(result).isGreaterThan(4);
+    }
+
 }
