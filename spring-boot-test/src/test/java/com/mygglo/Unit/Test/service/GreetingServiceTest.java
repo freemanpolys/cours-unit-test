62e4673a3e66dbb7b4992a721b6825ece628d5be
James Gaglo <freemanpolys@gmail.com>
2020-07-12 00:27:44 +0000

    We don't need SpringBootTest annotation for the unit test

--- spring-boot-test/src/test/java/com/mygglo/Unit/Test/service/GreetingServiceTest.java
+++ spring-boot-test/src/test/java/com/mygglo/Unit/Test/service/GreetingServiceTest.java
@@ -5,12 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-@SpringBootTest
 class GreetingServiceTest {
 
 
